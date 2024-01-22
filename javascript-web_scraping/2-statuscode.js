2cdd417d35b9c236bd57f4429b3eae3137e458ba
alex <6267@holbertonstudents.com>
2024-01-22 05:40:05 -0800

    correct statement

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
+
 request(url, function (error, response, body) {
-  if (!error) {
+  if (!error && response) {
     console.log('code:', response.statusCode);
   } else {
-    console.log('Error:', error);
+    console.error('Error:', error || 'Response object not available');
   }
 });
+
