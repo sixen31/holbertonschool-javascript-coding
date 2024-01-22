479c5e419b2f914b4cade2bb68eb6544f591bbc8
alex <6267@holbertonstudents.com>
2024-01-22 03:47:24 -0800

    correct fs.writeFile add contentToWrite after (filePath,

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(filePath, contentToWrite, 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
   } else {
