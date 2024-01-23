6e616e3fb3696f846bd405e6f392ce999658a022
alex <6267@holbertonstudents.com>
2024-01-23 02:58:43 -0800

    correct statement for check

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,6 +2,7 @@
 const request = require('request');
 const apiUrl = process.argv[2];
 const characterId = 18;
+
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
